2925c1cd8ecd88be8f7ed996b4e96ccc1547453a
svvx <github@svvx.at>
2019-08-19 12:26:23 +0800

    vc update

---
+++ src/vc6/二进制浮点数表示.py
@@ -0,0 +1,20 @@
+# 二进制浮点数表示
+if __name__ == '__main__':
+
+    num = 0.625
+
+    res = '0.'
+
+    count = 0
+
+    while num != 0.0000000:
+        num *= 2
+        if num >= 1:
+            res = res + str(int(num))
+            num -= 1;
+        else:
+            res = res + str(int(num))
+        if count > 33:
+            break;
+
+    pass
